6e2c86b8cdf8ac0b0210d9db7c5a96cbd0169eff
Thibault Goehringer <tgoehringer@gmail.com>
2020-09-10 21:14:43 +0200

    Implement Button

--- src/components/Button copy 6/Button.tsx
+++
@@ -1,7 +0,0 @@
-import { h, FunctionComponent } from 'preact';
-
-import style from './Button.css';
-
-const Button: FunctionComponent = () => <div className={style.button}></div>;
-
-export default Button;
--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,7 +1,21 @@
-import { h, FunctionComponent } from 'preact';
+import { h, FunctionComponent, ComponentChildren } from 'preact';
 
 import style from './Button.css';
 
-const Button: FunctionComponent = () => <div className={style.button}></div>;
+interface Props {
+  children: ComponentChildren;
+  disabled: boolean;
+  onClick: () => void;
+}
+
+const Button: FunctionComponent<Props> = ({
+  children = null,
+  disabled = false,
+  onClick,
+}: Props) => (
+  <button className={style.button} disabled={disabled} onClick={onClick}>
+    {children}
+  </button>
+);
 
 export default Button;
