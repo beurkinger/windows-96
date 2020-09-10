1b0dd469b41770b4589c9456f55db9360c3e7398
Thibault Goehringer <tgoehringer@gmail.com>
2020-09-10 21:38:48 +0200

    Add stacked prop to FieldRow

--- src/components/FieldRow/FieldRow.tsx
+++ src/components/FieldRow/FieldRow.tsx
@@ -4,10 +4,20 @@ import style from './FieldRow.css';
 
 interface Props {
   children: ComponentChildren;
+  stacked?: boolean;
 }
 
-const FieldRow: FunctionComponent<Props> = ({ children = null }: Props) => (
-  <div className={style.fieldrow}>{children}</div>
+const FieldRow: FunctionComponent<Props> = ({
+  children = null,
+  stacked = false,
+}: Props) => (
+  <div
+    className={`${style.fieldrow} ${
+      stacked ? 'field-row' : 'field-row-stacked'
+    }`}
+  >
+    {children}
+  </div>
 );
 
 export default FieldRow;
