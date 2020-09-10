78b51dda054f60129e69f5050b55a14a5dd101a5
Thibault Goehringer <tgoehringer@gmail.com>
2020-09-10 22:02:31 +0200

    Add multiline capabilities to Textbox component

--- src/components/TextBox/TextBox.tsx
+++ src/components/TextBox/TextBox.tsx
@@ -3,28 +3,36 @@ import { h, FunctionComponent, Fragment } from 'preact';
 import style from './Textbox.css';
 
 interface Props {
+  cols?: number;
   disabled?: boolean;
   id: string;
   label: string;
   onChange: (e: Event) => void;
+  rows?: number;
+  type?: 'text' | 'textarea';
   value: string;
 }
 
 const Textbox: FunctionComponent<Props> = ({
+  cols,
   disabled = false,
   id,
   label,
   onChange,
+  rows,
+  type = 'text',
   value,
 }: Props) => (
   <Fragment>
     <label htmlFor={id}>{label}</label>
     <input
       className={style.textbox}
+      cols={cols}
       disabled={disabled}
       id={id}
       onChange={onChange}
-      type="text"
+      rows={rows}
+      type={type}
       value={value}
     />
   </Fragment>
