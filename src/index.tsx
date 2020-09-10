daf4657797a0c1d0b126f56714f844165778b07a
Thibault Goehringer <tgoehringer@gmail.com>
2020-09-10 17:39:52 +0200

    Copied variables from 98.css to theme

--- src/index.tsx
+++ src/index.tsx
@@ -2,8 +2,8 @@ import { h, render } from 'preact';
 
 import App from './components/App/App';
 
-// import './theme/variables.css';
-// import './index.css';
 import '98.css/dist/98.css';
+import './theme/variables.css';
+import './index.css';
 
 render(<App />, document.body);
