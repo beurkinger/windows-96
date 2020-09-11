0c3fac15fbb9404e12a75bf0d2658a19d9c4a529
Thibault Goehringer <tgoehringer@gmail.com>
2020-09-11 11:35:33 +0200

    Implement Slider Component

--- src/components/Slider/Slider.tsx
+++ src/components/Slider/Slider.tsx
@@ -1,7 +1,62 @@
-import { h, FunctionComponent } from 'preact';
+import { h, FunctionComponent, Fragment, ComponentChild } from 'preact';
 
 import style from './Slider.css';
 
-const Slider: FunctionComponent = () => <div className={style.slider}></div>;
+interface Props {
+  min?: number;
+  disabled?: boolean;
+  hasBoxIndicator?: boolean;
+  id: string;
+  isVertical?: boolean;
+  label: string;
+  labelMax?: string;
+  labelMin?: string;
+  onChange: (e: Event) => void;
+  max?: number;
+  step?: number;
+  value: string;
+}
+
+const Slider: FunctionComponent<Props> = ({
+  min,
+  disabled = false,
+  hasBoxIndicator = false,
+  id,
+  isVertical = false,
+  label,
+  labelMax = '',
+  labelMin = '',
+  onChange,
+  max,
+  step,
+  value,
+}: Props) => {
+  const setVerticality = (input: ComponentChild) => {
+    return isVertical ? <div className="is-vertical">{input}</div> : input;
+  };
+
+  return (
+    <Fragment>
+      <label htmlFor={id}>{label}</label>
+      {labelMin && <label htmlFor={id}>{labelMin}</label>}
+      {setVerticality(
+        <input
+          className={`${style.slider} ${
+            hasBoxIndicator ? 'has-box-indicator' : ''
+          }`}
+          disabled={disabled}
+          id={id}
+          max={max}
+          min={min}
+          onChange={onChange}
+          step={step}
+          type="range"
+          value={value}
+        />
+      )}
+      {labelMax && <label htmlFor={id}>{labelMax}</label>}
+    </Fragment>
+  );
+};
 
 export default Slider;
