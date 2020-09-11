99d51813486085cd47a6d54e32439c3ce3c5c528
Thibault Goehringer <tgoehringer@gmail.com>
2020-09-11 12:39:02 +0200

    Implement TreeView component

--- src/components/TreeView/TreeView.tsx
+++ src/components/TreeView/TreeView.tsx
@@ -1,7 +1,36 @@
-import { h, FunctionComponent } from 'preact';
+import { h, FunctionComponent, ComponentChild, VNode } from 'preact';
 
-import style from './TreeeView.css';
+import style from './TreeView.css';
+interface Props {
+  items: ComponentChild[];
+  isExpandable?: boolean;
+  isOpen?: boolean;
+  summary?: string;
+}
 
-const TreeView: FunctionComponent = () => <div className={style.treeview}></div>;
+const TreeView: FunctionComponent<Props> = ({
+  items,
+  isExpandable,
+  isOpen = false,
+  summary,
+}: Props) => {
+  const setExpandability = (item: VNode<unknown> | null) => {
+    if (!isExpandable) return item;
+    return (
+      <details open={isOpen}>
+        <summary>{summary}</summary>
+        {item}
+      </details>
+    );
+  };
+
+  return setExpandability(
+    <ul className={`${style.TreeView} tree-view`}>
+      {items.map((item, i) => (
+        <li key={i}>{item}</li>
+      ))}
+    </ul>
+  );
+};
 
 export default TreeView;
