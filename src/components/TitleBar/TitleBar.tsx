ec3f1000f729f9b11e80e1dcfaa9c3b1a2bfe0b9
Thibault Goehringer <tgoehringer@gmail.com>
2020-09-11 12:14:26 +0200

    Implement TitleBar component

--- src/components/TitleBar/TitleBar.tsx
+++ src/components/TitleBar/TitleBar.tsx
@@ -2,6 +2,45 @@ import { h, FunctionComponent } from 'preact';
 
 import style from './TitleBar.css';
 
-const TitleBar: FunctionComponent = () => <div className={style.titlebar}></div>;
+interface Props {
+  isInactive: boolean;
+  isMaximized: boolean;
+  onClickMinimize?: () => void;
+  onClickMaximize?: () => void;
+  onClickRestore?: () => void;
+  onClickHelp?: () => void;
+  onClickClose: () => void;
+  text: string;
+}
+
+const TitleBar: FunctionComponent<Props> = ({
+  isInactive = false,
+  isMaximized = false,
+  onClickMinimize,
+  onClickMaximize,
+  onClickRestore,
+  onClickHelp,
+  onClickClose,
+  text,
+}: Props) => (
+  <div
+    className={`${style.titlebar} title-bar ${isInactive ? 'inactive' : ''}`}
+  >
+    <div className="title-bar-text">{text}</div>
+    <div className="title-bar-controls">
+      {onClickMinimize && (
+        <button aria-label="Minimize" onClick={onClickMinimize} />
+      )}
+      {!isMaximized && onClickMaximize && (
+        <button aria-label="Maximize" onClick={onClickMaximize} />
+      )}
+      {isMaximized && onClickRestore && (
+        <button aria-label="Restore" onClick={onClickRestore} />
+      )}
+      {onClickHelp && <button aria-label="Help" onClick={onClickHelp} />}
+      <button aria-label="Close" onClick={onClickClose} />
+    </div>
+  </div>
+);
 
 export default TitleBar;
