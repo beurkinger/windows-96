42e2191b5933cc139f46dbc7f21db3ec25d0b32f
Thibault Goehringer <tgoehringer@gmail.com>
2020-09-12 21:04:04 +0200

    Add status icons to NotificationArea

--- src/components/NotificationArea/NotificationArea.tsx
+++ src/components/NotificationArea/NotificationArea.tsx
@@ -1,9 +1,17 @@
 import { h, FunctionComponent } from 'preact';
 
+import Icon from '../Icon/Icon';
+
 import style from './NotificationArea.css';
 
 const NotificationArea: FunctionComponent = () => (
   <div className={style.notificationArea}>
+    <div className={style.statusIcon}>
+      <Icon />
+    </div>
+    <div className={style.statusIcon}>
+      <Icon />
+    </div>
     <div className={style.clock}>8: 05 PM</div>
   </div>
 );
