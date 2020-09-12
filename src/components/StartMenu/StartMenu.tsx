97715e0e19423b9b3349f151d0c733b0bc9e9ec5
Thibault Goehringer <tgoehringer@gmail.com>
2020-09-12 22:19:38 +0200

    Implement StartMenu

---
+++ src/components/StartMenu/StartMenu.tsx
@@ -0,0 +1,157 @@
+import { h, FunctionComponent } from 'preact';
+
+import Icon from '../Icon/Icon';
+import Menu from '../Menu/Menu';
+
+import style from './StartMenu.css';
+
+const StartMenu: FunctionComponent = () => (
+  <div className={style.startMenu}>
+    <Menu
+      onSelect={() => null}
+      isLarge={true}
+      options={[
+        [
+          {
+            icon: <Icon size={24} />,
+            label: 'Programs',
+            value: '',
+            // subMenu: {
+            //   isLarge: false,
+            //   options: [
+            //     [
+            //       { icon: <Icon />, label: 'Programs', value: '' },
+            //       { icon: <Icon />, label: 'Documents', value: '' },
+            //     ],
+            //     [{ icon: <Icon />, label: 'Settings', value: '' }],
+            //   ],
+            // },
+            subMenu: {
+              options: [
+                [
+                  {
+                    label: 'Accessories',
+                    icon: <Icon size={16} />,
+                    value: '',
+                  },
+                  {
+                    label: 'StartUp',
+                    icon: <Icon size={16} />,
+                    value: '',
+                  },
+                  {
+                    label: 'Microsoft Exchange',
+                    icon: <Icon size={16} />,
+                    value: '',
+                  },
+                  {
+                    label: 'MS-DOS Prompt',
+                    icon: <Icon size={16} />,
+                    value: '',
+                  },
+                  {
+                    label: 'The Microsoft Network',
+                    icon: <Icon size={16} />,
+                    value: '',
+                  },
+                  {
+                    label: 'Windows Explorer',
+                    icon: <Icon size={16} />,
+                    value: '',
+                  },
+                ],
+              ],
+            },
+          },
+          {
+            icon: <Icon size={24} />,
+            label: 'Documents',
+            value: '',
+            subMenu: {
+              options: [
+                [
+                  {
+                    label: 'Readme',
+                    icon: <Icon size={16} />,
+                    value: '',
+                  },
+                  {
+                    label: 'Document',
+                    icon: <Icon size={16} />,
+                    value: '',
+                  },
+                ],
+              ],
+            },
+          },
+          {
+            icon: <Icon size={24} />,
+            label: 'Settings',
+            value: '',
+            subMenu: {
+              options: [
+                [
+                  {
+                    label: 'Control Panel',
+                    icon: <Icon size={16} />,
+                    value: '',
+                  },
+                  {
+                    label: 'Printers',
+                    icon: <Icon size={16} />,
+                    value: '',
+                  },
+                  {
+                    label: 'Taskbar...',
+                    icon: <Icon size={16} />,
+                    value: '',
+                  },
+                ],
+              ],
+            },
+          },
+          {
+            icon: <Icon size={24} />,
+            label: 'Find',
+            value: '',
+            subMenu: {
+              options: [
+                [
+                  {
+                    label: 'Files or Folders...',
+                    icon: <Icon size={16} />,
+                    value: '',
+                  },
+                  {
+                    label: 'Computer...',
+                    icon: <Icon size={16} />,
+                    value: '',
+                  },
+                ],
+              ],
+            },
+          },
+          {
+            icon: <Icon size={24} />,
+            label: 'Help',
+            value: '',
+          },
+          {
+            icon: <Icon size={24} />,
+            label: 'Run',
+            value: '',
+          },
+        ],
+        [
+          {
+            icon: <Icon size={24} />,
+            label: 'Shutdown',
+            value: '',
+          },
+        ],
+      ]}
+    />
+  </div>
+);
+
+export default StartMenu;
--- src/components/Taskbar/Taskbar.tsx
+++ src/components/Taskbar/Taskbar.tsx
@@ -3,20 +3,24 @@ import { h, FunctionComponent } from 'preact';
 import Button from '../Button/Button';
 import Icon from '../Icon/Icon';
 import NotificationArea from '../NotificationArea/NotificationArea';
+import StartMenu from '../StartMenu/StartMenu';
 
 import style from './Taskbar.css';
 
 const Taskbar: FunctionComponent = () => (
   <div className={style.taskbar}>
-    <div className={style['start-button-wrapper']}>
+    <div className={style.startMenuWrapper}>
+      <StartMenu />
+    </div>
+    <div className={style.startButtonWrapper}>
       <Button icon={Icon} label="Start" onClick={() => null} />
     </div>
-    <div className={style['task-buttons-wrapper']}>
+    <div className={style.taskButtonsWrapper}>
       <Button icon={Icon} label="Notepad" onClick={() => null} />
       <Button icon={Icon} label="Paint" onClick={() => null} />
       <Button icon={Icon} label="Minesweeper" onClick={() => null} />
     </div>
-    <div className={style['notification-area-wrapper']}>
+    <div className={style.notificationAreaWrapper}>
       <NotificationArea />
     </div>
   </div>
