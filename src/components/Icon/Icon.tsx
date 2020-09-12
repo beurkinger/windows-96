0f50691dd8841f59b4106469359ec833c2b76fc0
Thibault Goehringer <tgoehringer@gmail.com>
2020-09-12 17:44:18 +0200

    Create first implementation of Icon component

---
+++ src/components/Icon/Icon.tsx
@@ -0,0 +1,16 @@
+import { h, FunctionComponent } from 'preact';
+
+import style from './Icon.css';
+
+interface Props {
+  size: 8 | 16 | 32;
+}
+
+const Icon: FunctionComponent<Props> = ({ size = 16 }: Props) => (
+  <div
+    className={style.icon}
+    style={{ height: `${size}px`, width: `${size}px` }}
+  />
+);
+
+export default Icon;
