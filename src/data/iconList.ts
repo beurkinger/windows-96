18f11ef0785a62ed389ab7a8051967fcbb37cbb8
Thibault Goehringer <tgoehringer@gmail.com>
2020-09-15 00:08:29 +0200

    Add new icons

--- src/data/iconList.ts
+++ src/data/iconList.ts
@@ -1,3 +1,7 @@
+import bin_empty_16 from '../assets/img/icons/bin_empty/bin_empty_16.png';
+import bin_empty_32 from '../assets/img/icons/bin_empty/bin_empty_32.png';
+import bin_full_16 from '../assets/img/icons/bin_full/bin_full_16.png';
+import bin_full_32 from '../assets/img/icons/bin_full/bin_full_32.png';
 import briefcase_16 from '../assets/img/icons/briefcase/briefcase_16.png';
 import briefcase_32 from '../assets/img/icons/briefcase/briefcase_32.png';
 import calc_16 from '../assets/img/icons/calc/calc_16.png';
@@ -26,8 +30,10 @@ import find_msn_16 from '../assets/img/icons/find_msn/find_msn_16.png';
 import find_msn_32 from '../assets/img/icons/find_msn/find_msn_32.png';
 import floppy_16 from '../assets/img/icons/floppy/floppy_16.png';
 import floppy_32 from '../assets/img/icons/floppy/floppy_32.png';
-import folder_16 from '../assets/img/icons/folder/folder_16.png';
-import folder_32 from '../assets/img/icons/folder/folder_32.png';
+import folder_closed_16 from '../assets/img/icons/folder_closed/folder_closed_16.png';
+import folder_closed_32 from '../assets/img/icons/folder_closed/folder_closed_32.png';
+import folder_open_16 from '../assets/img/icons/folder_open/folder_open_16.png';
+import folder_open_32 from '../assets/img/icons/folder_open/folder_open_32.png';
 import help_16 from '../assets/img/icons/help/help_16.png';
 import help_24 from '../assets/img/icons/help/help_24.png';
 import help_32 from '../assets/img/icons/help/help_32.png';
@@ -55,6 +61,8 @@ import notepad_doc_32 from '../assets/img/icons/notepad_doc/notepad_doc_32.png';
 import phone_dialer_16 from '../assets/img/icons/phone_dialer/phone_dialer_16.png';
 import printers_16 from '../assets/img/icons/printers/printers_16.png';
 import printers_32 from '../assets/img/icons/printers/printers_32.png';
+import program_16 from '../assets/img/icons/program/program_16.png';
+import program_32 from '../assets/img/icons/program/program_32.png';
 import programs_16 from '../assets/img/icons/programs/programs_16.png';
 import programs_24 from '../assets/img/icons/programs/programs_24.png';
 import programs_32 from '../assets/img/icons/programs/programs_32.png';
@@ -93,6 +101,8 @@ export type IconUrls = { 8: string; 16: string; 24: string; 32: string };
 export type IconList = { [key in IconId]: Partial<IconUrls> };
 
 export type IconId =
+  | 'binEmpty'
+  | 'binFull'
   | 'briefcase'
   | 'calc'
   | 'cdPlayer'
@@ -106,7 +116,8 @@ export type IconId =
   | 'findComputer'
   | 'findMsn'
   | 'floppy'
-  | 'folder'
+  | 'folderOpen'
+  | 'folderClosed'
   | 'help'
   | 'hyperterminal'
   | 'mediaPlayer'
@@ -121,6 +132,7 @@ export type IconId =
   | 'notepadDoc'
   | 'phoneDialer'
   | 'printers'
+  | 'program'
   | 'programs'
   | 'register'
   | 'run'
@@ -138,6 +150,8 @@ export type IconId =
   | 'wordpadDoc';
 
 export const iconList: IconList = {
+  binEmpty: { 16: bin_empty_16, 32: bin_empty_32 },
+  binFull: { 16: bin_full_16, 32: bin_full_32 },
   briefcase: { 16: briefcase_16, 32: briefcase_32 },
   calc: { 16: calc_16, 32: calc_32 },
   cdPlayer: { 16: cd_player_16, 32: cd_player_32 },
@@ -147,12 +161,12 @@ export const iconList: IconList = {
   exchange: { 16: exchange_16, 32: exchange_32 },
   explorer: { 16: explorer_16, 32: explorer_32 },
   documents: { 16: documents_16, 24: documents_24, 32: documents_32 },
-  programs: { 16: programs_16, 24: programs_24, 32: programs_32 },
   find: { 16: find_16, 24: find_24, 32: find_32 },
   findComputer: { 16: find_computer_16, 32: find_computer_32 },
   findMsn: { 16: find_msn_16, 32: find_msn_32 },
   floppy: { 16: floppy_16, 32: floppy_32 },
-  folder: { 16: folder_16, 32: folder_32 },
+  folderOpen: { 16: folder_open_16, 32: folder_open_32 },
+  folderClosed: { 16: folder_closed_16, 32: folder_closed_32 },
   help: { 16: help_16, 24: help_24, 32: help_32 },
   hyperterminal: { 16: hyperterminal_16, 32: hyperterminal_32 },
   mediaPlayer: { 16: media_player_16, 32: media_player_32 },
@@ -167,6 +181,8 @@ export const iconList: IconList = {
   notepadDoc: { 16: notepad_doc_16, 32: notepad_doc_32 },
   phoneDialer: { 16: phone_dialer_16 },
   printers: { 16: printers_16, 32: printers_32 },
+  program: { 16: program_16, 32: program_32 },
+  programs: { 16: programs_16, 24: programs_24, 32: programs_32 },
   register: { 16: register_16, 32: register_32 },
   run: { 16: run_16, 24: run_24, 32: run_32 },
   scandisk: { 16: scandisk_16, 32: scandisk_32 },
