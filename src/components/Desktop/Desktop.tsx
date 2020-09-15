56e70a90173e3e59d719b78ceda2272a0f798b23
Thibault Goehringer <tgoehringer@gmail.com>
2020-09-15 12:37:54 +0200

    Add focus to FileGrid icons

--- src/components/Desktop/Desktop.tsx
+++ src/components/Desktop/Desktop.tsx
@@ -1,7 +1,8 @@
 import { h, FunctionComponent } from 'preact';
-import { appList } from '../../data/appList';
+import { useState } from 'preact/hooks';
 
-import FileGrid from '../FileGrid/FileGrid';
+import { appList } from '../../data/appList';
+import FileGrid, { GridFile } from '../FileGrid/FileGrid';
 
 import style from './Desktop.css';
 
@@ -11,34 +12,82 @@ interface Props {
 
 const Desktop: FunctionComponent<Props> = ({
   background = 'lightseagreen',
-}: Props) => (
-  <div className={style.desktop} style={{ background }}>
-    <FileGrid
-      direction="column"
-      files={[
-        {
-          id: appList.myComputer.id,
-          iconId: appList.myComputer.iconId,
-          name: appList.myComputer.name,
-        },
-        {
-          id: appList.networkNeighborhood.id,
-          iconId: appList.networkNeighborhood.iconId,
-          name: appList.networkNeighborhood.name,
-        },
-        {
-          id: appList.briefcase.id,
-          iconId: appList.briefcase.iconId,
-          name: appList.briefcase.name,
-        },
-        {
-          id: appList.binEmpty.id,
-          iconId: appList.binEmpty.iconId,
-          name: appList.binEmpty.name,
-        },
-      ]}
-    />
-  </div>
-);
+}: Props) => {
+  const [files, setFiles] = useState<GridFile[]>([
+    {
+      id: appList.myComputer.id,
+      iconId: appList.myComputer.iconId,
+      hasFocus: false,
+      hasSoftFocus: true,
+      name: appList.myComputer.name,
+      type: 'app',
+    },
+    {
+      id: appList.networkNeighborhood.id,
+      iconId: appList.networkNeighborhood.iconId,
+      hasFocus: false,
+      hasSoftFocus: false,
+      name: appList.networkNeighborhood.name,
+      type: 'app',
+    },
+    {
+      id: appList.briefcase.id,
+      iconId: appList.briefcase.iconId,
+      hasFocus: false,
+      hasSoftFocus: false,
+      name: appList.briefcase.name,
+      type: 'app',
+    },
+    {
+      id: appList.binEmpty.id,
+      iconId: appList.binEmpty.iconId,
+      hasFocus: false,
+      hasSoftFocus: false,
+      name: appList.binEmpty.name,
+      type: 'app',
+    },
+  ]);
+
+  const handleOnClick = () => {
+    setFiles(
+      files.map((file) => ({
+        ...file,
+        hasFocus: false,
+        hasSoftFocus: file.hasFocus,
+      }))
+    );
+  };
+
+  const handleOnClickFile = (
+    fileId: string,
+    fileType: string,
+    e: MouseEvent
+  ) => {
+    e.preventDefault();
+    e.stopPropagation();
+    setFiles(
+      files.map((file) => ({
+        ...file,
+        hasFocus: file.type === fileType && file.id === fileId,
+        hasSoftFocus: file.type === fileType && file.id === fileId,
+      }))
+    );
+  };
+
+  return (
+    <div
+      className={style.desktop}
+      onClick={handleOnClick}
+      style={{ background }}
+    >
+      <FileGrid
+        direction="column"
+        files={files}
+        onClickFile={handleOnClickFile}
+        onDblClickFile={() => console.log('db click')}
+      />
+    </div>
+  );
+};
 
 export default Desktop;
--- src/components/FileGrid/FileGrid.tsx
+++ src/components/FileGrid/FileGrid.tsx
@@ -5,25 +5,40 @@ import Icon from '../Icon/Icon';
 
 import style from './FileGrid.css';
 
-interface File {
+export interface GridFile {
   id: string;
   iconId: IconId;
+  hasFocus: boolean;
+  hasSoftFocus: boolean;
   name: string;
+  type: 'app' | 'folder' | 'file';
 }
 
 interface Props {
-  files: File[];
   direction: 'column' | 'row';
+  onClickFile: (fileId: string, fileType: string, e: MouseEvent) => void;
+  onDblClickFile: (fileId: string, fileType: string, e: MouseEvent) => void;
+  files: GridFile[];
 }
 
 const FileGrid: FunctionComponent<Props> = ({
-  files,
   direction = 'row',
+  files,
+  onClickFile,
+  onDblClickFile,
 }: Props) => {
+  console.log(files);
   return (
     <div className={style.fileGrid} style={{ flexDirection: direction }}>
       {files.map((file, i) => (
-        <div className={style.file} key={i + file.id}>
+        <div
+          className={`${style.file} ${file.hasFocus ? style.focus : ''} ${
+            file.hasSoftFocus ? style.softFocus : ''
+          }`}
+          key={i + file.id}
+          onClick={(e) => onClickFile(file.id, file.type, e)}
+          onDblClick={(e) => onDblClickFile(file.id, file.type, e)}
+        >
           <div className={style.fileIcon}>
             <Icon iconId={file.iconId} size={32} />
           </div>
