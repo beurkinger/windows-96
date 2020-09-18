49c92b4476f445ca3e329151b139d2f0edc79113
Thibault Goehringer <tgoehringer@gmail.com>
2020-09-18 14:30:02 +0200

    Update way of importing icon images

--- src/data/iconList.ts
+++ src/data/iconList.ts
@@ -1,251 +1,107 @@
-import add_remove_programs_16 from '../assets/img/icons/add_remove_programs/add_remove_programs_16.png';
-import add_remove_programs_32 from '../assets/img/icons/add_remove_programs/add_remove_programs_32.png';
-import bin_empty_16 from '../assets/img/icons/bin_empty/bin_empty_16.png';
-import bin_empty_32 from '../assets/img/icons/bin_empty/bin_empty_32.png';
-import bin_full_16 from '../assets/img/icons/bin_full/bin_full_16.png';
-import bin_full_32 from '../assets/img/icons/bin_full/bin_full_32.png';
-import bmp_file_16 from '../assets/img/icons/bmp_file/bmp_file_16.png';
-import bmp_file_32 from '../assets/img/icons/bmp_file/bmp_file_32.png';
-import briefcase_16 from '../assets/img/icons/briefcase/briefcase_16.png';
-import briefcase_32 from '../assets/img/icons/briefcase/briefcase_32.png';
-import calc_16 from '../assets/img/icons/calc/calc_16.png';
-import calc_32 from '../assets/img/icons/calc/calc_32.png';
-import date_time_16 from '../assets/img/icons/date_time/date_time_16.png';
-import date_time_32 from '../assets/img/icons/date_time/date_time_32.png';
-import cd_drive_16 from '../assets/img/icons/cd_drive/cd_drive_16.png';
-import cd_drive_32 from '../assets/img/icons/cd_drive/cd_drive_32.png';
-import cd_player_16 from '../assets/img/icons/cd_player/cd_player_16.png';
-import cd_player_32 from '../assets/img/icons/cd_player/cd_player_32.png';
-import cd_track_32 from '../assets/img/icons/cd_track/cd_track_32.png';
-import control_panel_16 from '../assets/img/icons/control_panel/control_panel_16.png';
-import control_panel_32 from '../assets/img/icons/control_panel/control_panel_32.png';
-import defrag_16 from '../assets/img/icons/defrag/defrag_16.png';
-import defrag_32 from '../assets/img/icons/defrag/defrag_32.png';
-import dial_up_network_16 from '../assets/img/icons/dial_up_network/dial_up_network_16.png';
-import dial_up_network_32 from '../assets/img/icons/dial_up_network/dial_up_network_32.png';
-import documents_16 from '../assets/img/icons/documents/documents_16.png';
-import documents_24 from '../assets/img/icons/documents/documents_24.png';
-import documents_32 from '../assets/img/icons/documents/documents_32.png';
-import font_file_16 from '../assets/img/icons/font_file/font_file_16.png';
-import font_file_32 from '../assets/img/icons/font_file/font_file_32.png';
-import hard_drive_16 from '../assets/img/icons/hard_drive/hard_drive_16.png';
-import hard_drive_32 from '../assets/img/icons/hard_drive/hard_drive_32.png';
-import help_file_16 from '../assets/img/icons/help_file/help_file_16.png';
-import help_file_32 from '../assets/img/icons/help_file/help_file_32.png';
-import exchange_16 from '../assets/img/icons/exchange/exchange_16.png';
-import exchange_32 from '../assets/img/icons/exchange/exchange_32.png';
-import explorer_16 from '../assets/img/icons/explorer/explorer_16.png';
-import explorer_32 from '../assets/img/icons/explorer/explorer_32.png';
-import find_16 from '../assets/img/icons/find/find_16.png';
-import find_24 from '../assets/img/icons/find/find_24.png';
-import find_32 from '../assets/img/icons/find/find_32.png';
-import find_computer_16 from '../assets/img/icons/find_computer/find_computer_16.png';
-import find_computer_32 from '../assets/img/icons/find_computer/find_computer_32.png';
-import find_msn_16 from '../assets/img/icons/find_msn/find_msn_16.png';
-import find_msn_32 from '../assets/img/icons/find_msn/find_msn_32.png';
-import floppy_drive_16 from '../assets/img/icons/floppy_drive/floppy_drive_16.png';
-import floppy_drive_32 from '../assets/img/icons/floppy_drive/floppy_drive_32.png';
-import folder_closed_16 from '../assets/img/icons/folder_closed/folder_closed_16.png';
-import folder_closed_32 from '../assets/img/icons/folder_closed/folder_closed_32.png';
-import folder_open_16 from '../assets/img/icons/folder_open/folder_open_16.png';
-import folder_open_32 from '../assets/img/icons/folder_open/folder_open_32.png';
-import help_16 from '../assets/img/icons/help/help_16.png';
-import help_24 from '../assets/img/icons/help/help_24.png';
-import help_32 from '../assets/img/icons/help/help_32.png';
-import hyperterminal_16 from '../assets/img/icons/hyperterminal/hyperterminal_16.png';
-import hyperterminal_32 from '../assets/img/icons/hyperterminal/hyperterminal_32.png';
-import media_player_16 from '../assets/img/icons/media_player/media_player_16.png';
-import media_player_32 from '../assets/img/icons/media_player/media_player_32.png';
-import midi_file_16 from '../assets/img/icons/midi_file/midi_file_16.png';
-import midi_file_32 from '../assets/img/icons/midi_file/midi_file_32.png';
-import menu_arrow_8 from '../assets/img/icons/menu_arrow/menu_arrow_8.png';
-import msn_16 from '../assets/img/icons/msn/msn_16.png';
-import msn_32 from '../assets/img/icons/msn/msn_32.png';
-import ms_dos_16 from '../assets/img/icons/ms_dos/ms_dos_16.png';
-import ms_dos_32 from '../assets/img/icons/ms_dos/ms_dos_32.png';
-import ms_paint_16 from '../assets/img/icons/ms_paint/ms_paint_16.png';
-import ms_paint_32 from '../assets/img/icons/ms_paint/ms_paint_32.png';
-import music_16 from '../assets/img/icons/music/music_16.png';
-import music_32 from '../assets/img/icons/music/music_32.png';
-import my_computer_16 from '../assets/img/icons/my_computer/my_computer_16.png';
-import my_computer_32 from '../assets/img/icons/my_computer/my_computer_32.png';
-import network_16 from '../assets/img/icons/network/network_16.png';
-import network_32 from '../assets/img/icons/network/network_32.png';
-import notepad_16 from '../assets/img/icons/notepad/notepad_16.png';
-import notepad_32 from '../assets/img/icons/notepad/notepad_32.png';
-import notepad_doc_16 from '../assets/img/icons/notepad_doc/notepad_doc_16.png';
-import notepad_doc_32 from '../assets/img/icons/notepad_doc/notepad_doc_32.png';
-import phone_dialer_16 from '../assets/img/icons/phone_dialer/phone_dialer_16.png';
-import printers_16 from '../assets/img/icons/printers/printers_16.png';
-import printers_32 from '../assets/img/icons/printers/printers_32.png';
-import program_16 from '../assets/img/icons/program/program_16.png';
-import program_32 from '../assets/img/icons/program/program_32.png';
-import programs_16 from '../assets/img/icons/programs/programs_16.png';
-import programs_24 from '../assets/img/icons/programs/programs_24.png';
-import programs_32 from '../assets/img/icons/programs/programs_32.png';
-import register_16 from '../assets/img/icons/register/register_16.png';
-import register_32 from '../assets/img/icons/register/register_32.png';
-import quick_view_16 from '../assets/img/icons/quick_view/quick_view_16.png';
-import quick_view_32 from '../assets/img/icons/quick_view/quick_view_32.png';
-import run_16 from '../assets/img/icons/run/run_16.png';
-import run_24 from '../assets/img/icons/run/run_24.png';
-import run_32 from '../assets/img/icons/run/run_32.png';
-import scandisk_16 from '../assets/img/icons/scandisk/scandisk_16.png';
-import scandisk_32 from '../assets/img/icons/scandisk/scandisk_32.png';
-import settings_16 from '../assets/img/icons/settings/settings_16.png';
-import settings_24 from '../assets/img/icons/settings/settings_24.png';
-import settings_32 from '../assets/img/icons/settings/settings_32.png';
-import shutdown_16 from '../assets/img/icons/shutdown/shutdown_16.png';
-import shutdown_24 from '../assets/img/icons/shutdown/shutdown_24.png';
-import shutdown_32 from '../assets/img/icons/shutdown/shutdown_32.png';
-import sound_16 from '../assets/img/icons/sound/sound_16.png';
-import sound_32 from '../assets/img/icons/sound/sound_32.png';
-import sound_off_16 from '../assets/img/icons/sound_off/sound_off_16.png';
-import sound_off_32 from '../assets/img/icons/sound_off/sound_off_32.png';
-import taskbar_16 from '../assets/img/icons/taskbar/taskbar_16.png';
-import taskbar_32 from '../assets/img/icons/taskbar/taskbar_32.png';
-import tree_16 from '../assets/img/icons/tree/tree_16.png';
-import tree_32 from '../assets/img/icons/tree/tree_32.png';
-import video_16 from '../assets/img/icons/video/video_16.png';
-import video_32 from '../assets/img/icons/video/video_32.png';
-import video_file_16 from '../assets/img/icons/video_file/video_file_16.png';
-import video_file_32 from '../assets/img/icons/video_file/video_file_32.png';
-import volume_16 from '../assets/img/icons/volume/volume_16.png';
-import volume_32 from '../assets/img/icons/volume/volume_32.png';
-import warning_32 from '../assets/img/icons/warning/warning_32.png';
-import wave_file_16 from '../assets/img/icons/wave_file/wave_file_16.png';
-import wave_file_32 from '../assets/img/icons/wave_file/wave_file_32.png';
-import windows_logo_16 from '../assets/img/icons/windows_logo/windows_logo_16.png';
-import wordpad_16 from '../assets/img/icons/wordpad/wordpad_16.png';
-import wordpad_32 from '../assets/img/icons/wordpad/wordpad_32.png';
-import wordpad_doc_16 from '../assets/img/icons/wordpad_doc/wordpad_doc_16.png';
-import wordpad_doc_32 from '../assets/img/icons/wordpad_doc/wordpad_doc_32.png';
-
 export type IconSize = 8 | 16 | 24 | 32;
-export type IconUrls = { 8: string; 16: string; 24: string; 32: string };
-export type IconList = { [key in IconId]: Partial<IconUrls> };
+export type IconUrls = Partial<{
+  8: string;
+  16: string;
+  24: string;
+  32: string;
+}>;
+export type IconList = { [key in IconId]: IconUrls };
 
-export type IconId =
-  | 'addRemovePrograms'
-  | 'binEmpty'
-  | 'binFull'
-  | 'bmpFile'
-  | 'briefcase'
-  | 'calc'
-  | 'cdDrive'
-  | 'cdPlayer'
-  | 'cdTrack'
-  | 'controlPanel'
-  | 'dateTime'
-  | 'defrag'
-  | 'dialUpNetwork'
-  | 'documents'
-  | 'hardDrive'
-  | 'helpFile'
-  | 'exchange'
-  | 'explorer'
-  | 'find'
-  | 'findComputer'
-  | 'findMsn'
-  | 'floppyDrive'
-  | 'folderOpen'
-  | 'folderClosed'
-  | 'fontFile'
-  | 'help'
-  | 'hyperterminal'
-  | 'mediaPlayer'
-  | 'menuArrow'
-  | 'midiFile'
-  | 'msn'
-  | 'msDos'
-  | 'msPaint'
-  | 'music'
-  | 'myComputer'
-  | 'network'
-  | 'notepad'
-  | 'notepadDoc'
-  | 'phoneDialer'
-  | 'printers'
-  | 'program'
-  | 'programs'
-  | 'quickView'
-  | 'register'
-  | 'run'
-  | 'scandisk'
-  | 'settings'
-  | 'shutdown'
-  | 'sound'
-  | 'soundOff'
-  | 'taskbar'
-  | 'tree'
-  | 'video'
-  | 'videoFile'
-  | 'volume'
-  | 'warning'
-  | 'waveFile'
-  | 'windowsLogo'
-  | 'wordpad'
-  | 'wordpadDoc';
+const iconIds = [
+  'addRemovePrograms',
+  'binEmpty',
+  'test',
+  'binFull',
+  'bmpFile',
+  'briefcase',
+  'calc',
+  'cdDrive',
+  'cdPlayer',
+  'cdTrack',
+  'controlPanel',
+  'dateTime',
+  'defrag',
+  'dialUpNetwork',
+  'documents',
+  'hardDrive',
+  'helpFile',
+  'exchange',
+  'explorer',
+  'find',
+  'findComputer',
+  'findMsn',
+  'floppyDrive',
+  'folderOpen',
+  'folderClosed',
+  'fontFile',
+  'help',
+  'hyperterminal',
+  'mediaPlayer',
+  'menuArrow',
+  'midiFile',
+  'msn',
+  'msDos',
+  'msPaint',
+  'music',
+  'myComputer',
+  'network',
+  'notepad',
+  'notepadDoc',
+  'phoneDialer',
+  'printers',
+  'program',
+  'programs',
+  'quickView',
+  'register',
+  'run',
+  'scandisk',
+  'settings',
+  'shutdown',
+  'sound',
+  'soundOff',
+  'taskbar',
+  'tree',
+  'video',
+  'videoFile',
+  'volume',
+  'warning',
+  'waveFile',
+  'windowsLogo',
+  'wordpad',
+  'wordpadDoc',
+] as const;
 
-export const iconList: IconList = {
-  addRemovePrograms: { 16: add_remove_programs_16, 32: add_remove_programs_32 },
-  binEmpty: { 16: bin_empty_16, 32: bin_empty_32 },
-  binFull: { 16: bin_full_16, 32: bin_full_32 },
-  bmpFile: { 16: bmp_file_16, 32: bmp_file_32 },
-  briefcase: { 16: briefcase_16, 32: briefcase_32 },
-  calc: { 16: calc_16, 32: calc_32 },
-  cdDrive: { 16: cd_drive_16, 32: cd_drive_32 },
-  cdPlayer: { 16: cd_player_16, 32: cd_player_32 },
-  cdTrack: { 32: cd_track_32 },
-  controlPanel: { 16: control_panel_16, 32: control_panel_32 },
-  dateTime: { 16: date_time_16, 32: date_time_32 },
-  defrag: { 16: defrag_16, 32: defrag_32 },
-  dialUpNetwork: { 16: dial_up_network_16, 32: dial_up_network_32 },
-  hardDrive: { 16: hard_drive_16, 32: hard_drive_32 },
-  exchange: { 16: exchange_16, 32: exchange_32 },
-  explorer: { 16: explorer_16, 32: explorer_32 },
-  documents: { 16: documents_16, 24: documents_24, 32: documents_32 },
-  find: { 16: find_16, 24: find_24, 32: find_32 },
-  findComputer: { 16: find_computer_16, 32: find_computer_32 },
-  findMsn: { 16: find_msn_16, 32: find_msn_32 },
-  floppyDrive: { 16: floppy_drive_16, 32: floppy_drive_32 },
-  folderOpen: { 16: folder_open_16, 32: folder_open_32 },
-  folderClosed: { 16: folder_closed_16, 32: folder_closed_32 },
-  fontFile: { 16: font_file_16, 32: font_file_32 },
-  help: { 16: help_16, 24: help_24, 32: help_32 },
-  helpFile: { 16: help_file_16, 32: help_file_32 },
-  hyperterminal: { 16: hyperterminal_16, 32: hyperterminal_32 },
-  mediaPlayer: { 16: media_player_16, 32: media_player_32 },
-  menuArrow: { 8: menu_arrow_8 },
-  midiFile: { 16: midi_file_16, 32: midi_file_32 },
-  msn: { 16: msn_16, 32: msn_32 },
-  msDos: { 16: ms_dos_16, 32: ms_dos_32 },
-  msPaint: { 16: ms_paint_16, 32: ms_paint_32 },
-  music: { 16: music_16, 32: music_32 },
-  myComputer: { 16: my_computer_16, 32: my_computer_32 },
-  network: { 16: network_16, 32: network_32 },
-  notepad: { 16: notepad_16, 32: notepad_32 },
-  notepadDoc: { 16: notepad_doc_16, 32: notepad_doc_32 },
-  phoneDialer: { 16: phone_dialer_16 },
-  printers: { 16: printers_16, 32: printers_32 },
-  program: { 16: program_16, 32: program_32 },
-  programs: { 16: programs_16, 24: programs_24, 32: programs_32 },
-  quickView: { 16: quick_view_16, 32: quick_view_32 },
-  register: { 16: register_16, 32: register_32 },
-  run: { 16: run_16, 24: run_24, 32: run_32 },
-  scandisk: { 16: scandisk_16, 32: scandisk_32 },
-  settings: { 16: settings_16, 24: settings_24, 32: settings_32 },
-  shutdown: { 16: shutdown_16, 24: shutdown_24, 32: shutdown_32 },
-  sound: { 16: sound_16, 32: sound_32 },
-  soundOff: { 16: sound_off_16, 32: sound_off_32 },
-  taskbar: { 16: taskbar_16, 32: taskbar_32 },
-  tree: { 16: tree_16, 32: tree_32 },
-  video: { 16: video_16, 32: video_32 },
-  videoFile: { 16: video_file_16, 32: video_file_32 },
-  volume: { 16: volume_16, 32: volume_32 },
-  warning: { 32: warning_32 },
-  waveFile: { 16: wave_file_16, 32: wave_file_32 },
-  windowsLogo: { 16: windows_logo_16 },
-  wordpad: { 16: wordpad_16, 32: wordpad_32 },
-  wordpadDoc: { 16: wordpad_doc_16, 32: wordpad_doc_32 },
-};
+export type IconId = typeof iconIds[number];
+
+function importAll(r: __WebpackModuleApi.RequireContext) {
+  const cache = {} as IconList;
+  r.keys().forEach((key) => {
+    const filePath: string = r(key);
+    const matches = key.match(/\/(\w*?)\/(\w*?)_(\d*?)\.png/);
+    const iconId = matches ? (matches[1] as IconId) : '';
+    const size = matches ? (parseInt(matches[3]) as IconSize) : '';
+    if (iconId && size) {
+      const iconUrls: IconUrls = cache[iconId]
+        ? {
+            ...cache[iconId],
+            [size]: filePath,
+          }
+        : {
+            [size]: filePath,
+          };
+      cache[iconId as IconId] = iconUrls;
+    }
+  });
+  const error = iconIds.some((iconId) => {
+    if (!cache[iconId])
+      console.error(
+        `IconId "${iconId}" doesn't have an associated icon folder.`
+      );
+    return !cache[iconId];
+  });
+  if (error) throw "Some IconIds don't have an associated icon folder";
+  return cache;
+}
+export const iconList = importAll(
+  require.context('../assets/img/icons', true, /\.png$/)
+);
