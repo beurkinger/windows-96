9c6a54fa62aa28027eda6e325896cf5b0bd00d4e
Thibault Goehringer <tgoehringer@gmail.com>
2020-09-22 12:47:22 +0200

    Update TimerApp

--- src/components/apps/TimerApp/TimerApp.tsx
+++ src/components/apps/TimerApp/TimerApp.tsx
@@ -1,69 +1,80 @@
 import { h, FunctionComponent } from 'preact';
+import { useRef, useState } from 'preact/hooks';
 
 import happy from '../../../assets/img/interface/minesweeper_happy.png';
 // import cool from '../../../assets/img/interface/minesweeper_cool.png';
 // import sick from '../../../assets/img/interface/minesweeper_sick.png';
 // import dead from '../../../assets/img/interface/minesweeper_dead.png';
 
-import n0 from '../../../assets/img/interface/minesweeper_0.png';
-// import n1 from '../../../assets/img/interface/minesweeper_1.png';
-// import n2 from '../../../assets/img/interface/minesweeper_2.png';
-// import n3 from '../../../assets/img/interface/minesweeper_3.png';
-// import n4 from '../../../assets/img/interface/minesweeper_4.png';
-// import n5 from '../../../assets/img/interface/minesweeper_5.png';
-// import n6 from '../../../assets/img/interface/minesweeper_6.png';
-// import n7 from '../../../assets/img/interface/minesweeper_7.png';
-// import n8 from '../../../assets/img/interface/minesweeper_8.png';
-// import n9 from '../../../assets/img/interface/minesweeper_9.png';
-
 import { AppProps } from '../../../types/App';
 import Bump from '../../shared/Bump/Bump';
 import Button from '../../shared/Button/Button';
-// import MenuBar from '../../shared/MenuBar/MenuBar';
-// import StatusBar from '../../shared/StatusBar/StatusBar';
+import TimerNumber from '../../shared/TimerNumber/TimerNumber';
 import WindowContent from '../../shared/WindowContent/WindowContent';
 
 import style from './TimerApp.css';
 
-const TimerApp: FunctionComponent<AppProps> = () => (
-  <WindowContent
-    // menu={<MenuBar options={['Game', 'Help']} />}
-    body={
-      <Bump size="large" type="outer">
-        <div className={style.timerAppContent}>
-          <Bump size="medium" type="inner">
-            <div className={style.timers}>
-              <Bump size="small" type="inner">
-                <div className={style.timer}>
-                  <img src={n0} />
-                  <img src={n0} />
-                  <img src={n0} />
-                </div>
-              </Bump>
-              <Button label={<img src={happy} />} onClick={() => null} />
-              <Bump size="small" type="inner">
-                <div className={style.timer}>
-                  <img src={n0} />
-                  <img src={n0} />
-                  <img src={n0} />
-                </div>
-              </Bump>
-              <Bump size="small" type="inner">
-                <div className={style.timer}>
-                  <img src={n0} />
-                  <img src={n0} />
-                  <img src={n0} />
-                </div>
-              </Bump>
-            </div>
-          </Bump>
-        </div>
-      </Bump>
-    }
-    // footer={
-    // <StatusBar textRight="To edit, click Open File for Editing on the File menu." />
-    // }
-  />
-);
+const parseNumber = (i: number): string[] => ('000' + i).slice(-3).split('');
+
+const getCurrentCountdown = (targetMs: number) => {
+  const diff = targetMs - Date.now();
+
+  let tmp = Math.floor(diff / 1000);
+  const sec = tmp % 60;
+
+  tmp = Math.floor((tmp - sec) / 60);
+  const min = tmp % 60;
+
+  tmp = Math.floor((tmp - min) / 60);
+  const hour = tmp;
+
+  return {
+    h: parseNumber(hour),
+    m: parseNumber(min),
+    s: parseNumber(sec),
+  };
+};
+
+const TimerApp: FunctionComponent<AppProps> = () => {
+  const targetMs = useRef(Date.now() + 1000 * 60 * 60 * 24 * 5);
+  const [countdown] = useState(getCurrentCountdown(targetMs.current));
+  return (
+    <WindowContent
+      body={
+        <Bump size="large" type="outer">
+          <div className={style.timerAppContent}>
+            <Bump size="medium" type="inner">
+              <div className={style.timers}>
+                <Bump size="small" type="inner">
+                  <div className={style.timer}>
+                    <TimerNumber i={parseInt(countdown['h'][0])} />
+                    <TimerNumber i={parseInt(countdown['h'][1])} />
+                    <TimerNumber i={parseInt(countdown['h'][2])} />
+                  </div>
+                </Bump>
+                <Button label={<img src={happy} />} onClick={() => null} />
+                <Bump size="small" type="inner">
+                  <div className={style.timer}>
+                    <TimerNumber i={parseInt(countdown['m'][0])} />
+                    <TimerNumber i={parseInt(countdown['m'][1])} />
+                    <TimerNumber i={parseInt(countdown['m'][2])} />
+                  </div>
+                </Bump>
+                <Button label={<img src={happy} />} onClick={() => null} />
+                <Bump size="small" type="inner">
+                  <div className={style.timer}>
+                    <TimerNumber i={parseInt(countdown['s'][0])} />
+                    <TimerNumber i={parseInt(countdown['s'][1])} />
+                    <TimerNumber i={parseInt(countdown['s'][2])} />
+                  </div>
+                </Bump>
+              </div>
+            </Bump>
+          </div>
+        </Bump>
+      }
+    />
+  );
+};
 
 export default TimerApp;
